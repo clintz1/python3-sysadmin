b64f88e35016f39cb278112e8fb5c7980d9d9895
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-22 15:41:48 +0800

    Interacting with Files

---
+++ basic_script/interact_file.py
@@ -0,0 +1,37 @@
+# Opening and Reading a File
+xmen_file = open('xmen_base.txt', 'r')
+print(xmen_file)
+print(xmen_file.read())
+print(xmen_file.read())
+print(xmen_file.seek(0))
+print(xmen_file.read())
+print(xmen_file.seek(6))
+print(xmen_file.read())
+xmen_file.seek(0)
+for line in xmen_file:
+    print(line, end="")
+xmen_file.close()
+
+# Creating a New File and Writing to It
+xmen_base = open('xmen_base.txt')
+new_xmen = open('new_xmen.txt', 'w')
+new_xmen.write(xmen_base.read())
+new_xmen.close()
+new_xmen = open(new_xmen.name, 'r+')
+print(new_xmen.read())
+print(new_xmen.seek(0))
+print(new_xmen.write("Beast\n"))
+print(new_xmen.write("Phoenix\n"))
+print(new_xmen.seek(0))
+print(new_xmen.read())
+
+# Appending to a File
+xmen_file.close()
+with open('xmen_base.txt', 'a') as f:
+    f.write('Professor Xavier\n')
+
+f = open('xmen_base.txt', 'a')
+with f:
+    f.write("Something\n")
+
+exit()
