1f7521d0566417867ef62dde42fb00703c9c0355
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-22 15:32:09 +0800

    Working with Environment Variables

---
+++ basic_script/env_var.py
@@ -0,0 +1,15 @@
+# Working with Environment Variables
+import os
+
+
+stage = os.getenv("STAGE", "dev").upper()
+
+
+output = f"We're running in {stage}"
+
+
+if stage.startswith("PROD"):
+    output = "DANGER!!! - " + output
+
+
+print(output)
