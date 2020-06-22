7e73cf38665aeefa43b90689f55586b8342ea58e
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-22 13:53:02 +0800

    Using Comments

---
+++ run_py/use_comment.py
@@ -0,0 +1,8 @@
+# This is a full like comment
+print(2 + 2) # This will add the number
+
+"""
+This is not a block comment,
+but it will still work when you really need
+for some lines of code to not execute.
+"""
