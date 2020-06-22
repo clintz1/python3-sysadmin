fffa2593ca0464b9fed294cb65fda2210ae3f7fe
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-22 19:40:01 +0800

    Implementing CLI Guided By Tests

--- pgbackup/tests/test_cli.py
+++ pgbackup/tests/test_cli.py
@@ -1,34 +1,52 @@
+# Setting system path before first test
+import sys
+path = "\\".join(sys.path[0].split('\\\\')[:-1])
+sys.path.append(path)
+
 # Writing Our First Tests
 import pytest
-
 from pgbackup import cli
 
 url = "postgres://postgres:postgres@127.0.0.1:5432/sample"
 
-def test_parser_without_driver():
+@pytest.fixture
+def parser():
+    return cli.create_parser()
+
+def test_parser_without_driver(parser):
     """
     Without a specified driver the parser will exit
     """
     with pytest.raises(SystemExit):
-        parser = cli.create_parser()
         parser.parse_args([url])
 
-def test_parser_with_driver():
+def test_parser_with_driver(parser):
     """
     The parser will exit if it receives a driver
     without a destination
     """
-    parser = cli.create_parser()
     with pytest.raises(SystemExit):
         parser.parse_args([url, "--driver", "local"])
 
-def test_parser_with_driver_and_destination():
+def test_parser_with_driver_and_destination(parser):
     """
     The parser will not exit if it receives a driver
     with a destination
     """
-    parser = cli.create_parser()
-
     args = parser.parse_args([url, "--driver","local","/some/path"]) 
     assert args.driver == "local"
     assert args.destination == "/some/path"
+
+def test_parser_with_unknown_drivers(parser):
+    """
+    The parser will exit if the driver name is unknown.
+    """
+    with pytest.raises(SystemExit):
+        parser.parse_args([url, "--driver", "azure", "destination"])
+
+def test_parser_with_known_drivers(parser):
+    """
+    The parser will not exit if the driver name is known.
+    """
+    for driver in ['local', 's3']:
+        assert parser.parse_args([url, "--driver", driver, "destination"])
