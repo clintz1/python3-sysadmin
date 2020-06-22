5599ef0e0a27fc2332f402702bbbc5c4b772c6dd
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-22 15:55:52 +0800

    Robust CLIs with 'argparse' - Part 1

---
+++ mid_script/argparse_1.py
@@ -0,0 +1,11 @@
+# Building a CLI to Reverse Files
+import argparse
+
+parser = argparse.ArgumentParser(description='Read a file in reverse')
+parser.add_argument('filename', help='the file to read')
+parser.add_argument('--limit', '-l', type=int, help='the number of lines to read')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+
+
+args = parser.parse_args()
+print(args)
