f6823a2884f764a229a8230df35438e2463b0820
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-22 14:11:24 +0800

    Working with Variables

---
+++ common_dt/work_var.py
@@ -0,0 +1,13 @@
+# Working with Variables
+my_str = "This is a simple string"
+print(my_str)
+my_str += " testing"
+print(my_str)
+my_str = 1
+print(my_str)
+
+my_str = 1
+my_int = my_str
+my_str = "testing"
+print(my_int)
+print(my_str)
