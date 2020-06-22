4ea7b84d28a13e1beecdbccbd66caf7e5bd35473
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-22 19:49:04 +0800

    Implementing Local File Storage

--- pgbackup/storage.py
+++ pgbackup/storage.py
@@ -3,3 +3,6 @@ def local(infile, outfile):
     outfile.write(infile.read())
     outfile.close()
     infile.close()
+
+def s_3(client, infile, bucket, name):
+    client.upload_fileobj(infile, bucket, name)
--- pgbackup/tests/test_storage.py
+++ pgbackup/tests/test_storage.py
@@ -5,8 +5,17 @@ sys.path.append(path)
 
 # Writing Our Tests
 import tempfile
+import pytest
 
 from pgbackup import storage
+
+
+@pytest.fixture
+def infile():
+    infile = tempfile.TemporaryFile('r+b')
+    infile.write(b"Testing")
+    infile.seek(0)
+    return infile
 
 
 def test_storing_file_locally():
@@ -21,3 +30,18 @@ def test_storing_file_locally():
     storage.local(infile, outfile)
     with open(outfile.name, 'rb') as f:
         assert f.read() == b"Testing"
+
+def test_storing_file_on_s3(mocker, infile):
+    """
+    Writes content from one readable to S3
+    """
+    client = mocker.Mock()
+
+    storage.s_3(client,
+            infile,
+            "bucket",
+            "file-name")
+    client.upload_fileobj.assert_called_with(
+            infile,
+            "bucket",
+            "file-name")
