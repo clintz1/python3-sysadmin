89abed94f9195d78452cd854b728de3140bb465f
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-22 15:23:21 +0800

    Function Basics

---
+++ basic_script/func_basic.py
@@ -0,0 +1,16 @@
+# Function Basics
+def hello_world():
+    print("Hello, World!")
+
+hello_world()
+def print_name(name):
+    print(f"Name is {name}")
+
+print_name("Keith")
+output = print_name("Keith")
+print(output)
+def add_two(num):
+    return num + 2
+
+result = add_two(2)
+print(result)
