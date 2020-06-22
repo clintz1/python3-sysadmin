381429cc66e6ed1a04cf843d18c1e73d9b5bf4e1
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-22 15:10:04 +0800

    Creating and Displaying Variables

---
+++ control_flow/display_var.py
@@ -0,0 +1,7 @@
+first_name = "Kevin"
+last_name = "Bacon"
+age = 59
+birth_date = "07/08/1958"
+
+print(f"My name is {first_name} {last_name}.")
+print(f"I was born on {birth_date}, and I'm {age} years old.")
