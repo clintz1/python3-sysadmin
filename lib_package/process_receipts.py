1750a6b87e5f96195833d7064abcc9b176ce54c9
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-22 17:55:44 +0800

    re & math

--- lib_package/process_receipts.py
+++ lib_package/process_receipts.py
@@ -10,18 +10,17 @@ except OSError:
     print("'processed' directory already exists")
 
 # Get a list of receipts
-receipts = glob.glob('./new/receipt-[0-9]*.json')
+receipts = glob.iglob('./new/receipt-[0-9]*.json')
 receipts = [r.replace('\\', '/') for r in receipts]
-print(receipts)
 subtotal = 0.0
 
 for path in receipts:
     with open(path) as f:
         content = json.load(f)
         subtotal += float(content['value'])
-    name = path.split('/')[-1]
-    destination = f"./processed/{name}"
+    destination = path.replace('new', 'processed')
     shutil.move(path, destination)
     print(f"moved '{path}' to '{destination}'")
 
-print("Receipt subtotal: $%.2f" % subtotal)
+
+print(f"Receipt subtotal: ${round(subtotal, 2)}")
