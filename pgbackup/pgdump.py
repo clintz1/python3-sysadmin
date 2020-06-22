dab1477e14f9299d49c9e6c1e92551080acf8cf8
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-22 19:45:14 +0800

    Implementing PostgreSQL Interaction

---
+++ pgbackup/pgdump.py
@@ -0,0 +1,10 @@
+# Initial Implementation
+import sys
+import subprocess
+
+def dump(url):
+    try:
+        return subprocess.Popen(['pg_dump', url], stdout=subprocess.PIPE)
+    except OSError as err:
+        print(f"Error: {err}")
+        sys.exit(1)
--- pgbackup/tests/test_pgdump.py
+++ pgbackup/tests/test_pgdump.py
@@ -9,7 +9,6 @@ import subprocess
 from pgbackup import pgdump
 url = "postgres://postgres:postgres@127.0.0.1:5432/sample"
 
-
 def test_dump_calls_pg_dump(mocker):
     """
     Utilize pg_dump with the database URL
@@ -17,3 +16,12 @@ def test_dump_calls_pg_dump(mocker):
     mocker.patch('subprocess.Popen')
     assert pgdump.dump(url)
     subprocess.Popen.assert_called_with(['pg_dump', url], stdout=subprocess.PIPE)
+
+
+def test_dump_handles_oserror(mocker):
+    """
+    pgdump.dump returns a reasonable error if pg_dump isn't installed.
+    """
+    mocker.patch('subprocess.Popen', side_effect=OSError("no such file"))
+    with pytest.raises(SystemExit):
+        pgdump.dump(url)
